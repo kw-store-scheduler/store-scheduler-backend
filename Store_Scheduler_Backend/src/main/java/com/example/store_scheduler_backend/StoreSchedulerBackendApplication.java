8733a6232dc00091bda0b750540d5e701ac866f0
aura_230 <hhj3497@naver.com>
2026-05-13 02:32:59 +0900

    초기 환경 설정 및 DB 연동 완료

---
+++ Store_Scheduler_Backend/src/main/java/com/example/store_scheduler_backend/StoreSchedulerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.store_scheduler_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreSchedulerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreSchedulerBackendApplication.class, args);
+    }
+
+}
---
+++ Store_Scheduler_Backend/src/test/java/com/example/store_scheduler_backend/StoreSchedulerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.store_scheduler_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreSchedulerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
